1d4eafffd2479c707be1cbdd7667550962466f9e
Nazim <nazim.aghabayov@buta-tech.com>
2021-11-25 14:57:52 +0100

    Initial commit

---
+++ app/src/main/java/com/alphawallet/app/entity/MediaLinks.java
@@ -0,0 +1,20 @@
+package com.alphawallet.app.entity;
+
+public class MediaLinks
+{
+    public static final String AWALLET_TELEGRAM_URL = "https://t.me/ClouchLab";
+    public static final String AWALLET_DISCORD_URL = null;
+    public static final String AWALLET_TWITTER_ID = "twitter://user?user_id=938624096123764736";
+    public static final String AWALLET_FACEBOOK_ID = "fb://page/1958651857482632";
+    public static final String AWALLET_TWITTER_URL = "https://twitter.com/ClouchLab";
+    public static final String AWALLET_FACEBOOK_URL = "https://business.facebook.com/Clouch-108581114917579";
+    public static final String AWALLET_LINKEDIN_URL = "https://www.linkedin.com/company/clouch/";
+    public static final String AWALLET_REDDIT_URL = null;
+    public static final String AWALLET_INSTAGRAM_URL = "https://www.instagram.com/clouchlab/";
+    public static final String AWALLET_BLOG_URL = null;
+    public static final String AWALLET_FAQ_URL = "https://clouch.com/faq/";
+
+    public static final String AWALLET_EMAIL1 = "clouch";
+    public static final String AWALLET_EMAIL2 = "clouch.com";
+    public static final String AWALLET_SUBJECT = "Clouch Wallet Android Help";
+}
